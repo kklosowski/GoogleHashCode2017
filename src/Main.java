3ce17712c630c0f05b6c3131f200eba356fb2c99
Oskar F <ofigura@interia.pl>
2018-02-13 19:07:04 +0000

    t

--- src/Main.java
+++ src/Main.java
@@ -9,26 +9,30 @@ import java.util.stream.Collectors;
  * created on: 13/02/2018.
  */
 public class Main {
-    public static final String FILE_SIZE = "kittens";
+    public static final String FILE_SIZE = "me_at_the_zoo";
     public static void main(String[] args) {
 
         try {
             List<String> raw = Files.lines(Paths.get("../StreamingVideo/src/"+FILE_SIZE+".in")).collect(Collectors.toList());
 
-            String[] details = raw.get(0).split(" ");
+            String[] data = raw.get(0).split(" ");
             raw.remove(0);
 
-            int HEIGHT = Integer.valueOf(details[0]);
-            int WIDTH = Integer.valueOf(details[1]);
-            int MIN = Integer.valueOf(details[2]);
-            int MAX = Integer.valueOf(details[3]);
+            int videoNo = Integer.valueOf(data[0]);
+            int endpointNo = Integer.valueOf(data[1]);
+            int requestNo = Integer.valueOf(data[2]);
+            int cacheNo = Integer.valueOf(data[3]);
+            int cacheSize = Integer.valueOf(data[4]);
 
+
+            /*
             char[][] parsed = new char[WIDTH][HEIGHT];
             for (int i = 0; i < WIDTH; i++) {
                 for (int j = 0; j < HEIGHT; j++) {
                     parsed[i][j] = raw.get(j).charAt(i);
                 }
             }
+            */
         } catch (Exception e) {
             e.printStackTrace();
         }
